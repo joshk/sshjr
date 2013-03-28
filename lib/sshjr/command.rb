177452ecaff4a90f6038ae7fe9adaffd459fc9d1
Henrik Hodne <me@henrikhodne.com>
2013-03-27 21:42:19 -0500

    Fix nonblock issues

--- lib/sshjr/command.rb
+++ lib/sshjr/command.rb
@@ -43,7 +43,8 @@ module SSHJr
     def process_output_io(io)
       output_str = io.read_nonblock(1024)
       @on_output.each { |cb| cb.call(output_str) }
-    rescue EOFError
+    rescue EOFError, Errno::EWOULDBLOCK, Errno::EAGAIN
+      retry if IO.select([io], nil, nil, 1.0)
     end
   end
 end
