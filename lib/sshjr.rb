f9d5a30f1c6d240fa6d47de7341ef007b5b90029
Henrik Hodne <me@henrikhodne.com>
2013-03-27 20:30:35 -0500

    Update dependencies

--- lib/sshjr.rb
+++ lib/sshjr.rb
@@ -1,7 +1,7 @@
 require 'java'
 require 'vendor/slf4j-api'
-require 'vendor/bcprov-jdk15on-147'
-require 'vendor/bcpkix-jdk15on-147'
+require 'vendor/bcprov-jdk15on-148'
+require 'vendor/bcpkix-jdk15on-148'
 require 'vendor/sshj'
 
 module SSHJr
--- lib/sshjr/vendor.rb
+++ lib/sshjr/vendor.rb
@@ -1,7 +1,7 @@
 require "java"
 
 require "vendor/slf4j-api.jar"
-require 'vendor/bcprov-jdk15on-147'
-require 'vendor/bcprov-ext-jdk15on-147'
-require 'vendor/bcpkix-jdk15on-147'
+require 'vendor/bcprov-jdk15on-148'
+require 'vendor/bcprov-ext-jdk15on-148'
+require 'vendor/bcpkix-jdk15on-148'
 require "vendor/sshj.jar"
